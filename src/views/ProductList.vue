2900f2d6beb7fbe5efb9ecf3b7c3cb52fdd4b0e5
Alicia Delgado <aliciajf1215@yahoo.com.br>
2025-02-12 19:49:03 -0300

    Relocating products to parent component

--- src/views/ProductList.vue
+++ src/views/ProductList.vue
@@ -1,2 +1,22 @@
-
+<template>
+    <div class="product-list">
+      <ImageList :type="$route.params.type" />
+    </div>
+  </template>
+  
+  <script>
+  import ImageList from "@/components/ImageList.vue";
+  
+  export default {
+    components: {
+      ImageList,
+    },
+  };
+  </script>
+  
+  <style lang="less" scoped>
+  .product-list {
+    padding: 1rem;
+  }
+  </style>
   
