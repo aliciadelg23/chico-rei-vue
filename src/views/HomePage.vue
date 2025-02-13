6d1e37c98e8d8bc4642893cd020214e18b8d84ba
Alicia Delgado <aliciajf1215@yahoo.com.br>
2025-02-13 10:22:48 -0300

    Inserting images for homepage

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,17 +1,46 @@
 <template>
   <div class="home">
     <h1>Camisetas</h1>
-  </div>
-</template>
+    <img
+      src="https://chico-rei.imgix.net/images/site/home/banners/a76c2f60-e4ea-11ef-87b6-f3921051aa48.png?auto=compress,format&q=65"
+      alt="Promoção 1"
+      class="banner"
+    />
+    <img
+      src="https://chico-rei.imgix.net/images/site/home/banners/1145e580-e4bd-11ef-b3f8-f1d615184870.png?auto=compress,format&q=65"
+      alt="Promoção 2"
+      class="banner"
+    />
 
-<script>
-export default {
-  name: "HomePage",
-};
-</script>
+    <img
+      src=" https://chico-rei.imgix.net/images/site/home/banners/3f101350-d748-11ef-a234-799949f4a9d3.png?auto=compress,format&q=65"
+      alt="Promoção 3"
+      class="banner"
+      />
+
+      <img
+      src="  https://chico-rei.imgix.net/images/site/home/banners/86e55be0-cc21-11ef-a02e-f9a2cda5373d.png?auto=compress,format&q=65"
+      alt="Promoção 4"
+      class="banner"
+      />
+
+      
+
+   
+  </div>
+
+
+ 
+</template>
 
 <style lang="less" scoped>
 .home {
   text-align: center;
+}
+
+.banner {
+  max-width: 100%;
+  height: auto;
+  margin-top: 20px;
 }
 </style>
